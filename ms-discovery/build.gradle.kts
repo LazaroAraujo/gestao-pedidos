713cd0b8257e5c8bbdb17257e2bf0dcd046a6b2a
Lazaro Araujo <lazaroaraujosilva@gmail.com>
2025-08-23 23:27:14 -0300

    Estrutura inicial do service discovery

--- ms-discovery/build.gradle.kts
+++ ms-discovery/build.gradle.kts
@@ -1,20 +1,39 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.5.5"
+    id("io.spring.dependency-management") version "1.1.7"
 }
+val springCloudVersion by extra("2025.0.0")
 
 group = "br.com.dio"
 version = "0.0.1-SNAPSHOT"
+description = "ms-discovery"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
+}
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
